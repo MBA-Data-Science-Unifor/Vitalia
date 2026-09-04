43f3cdad9019a5034fa1404fc6df4e30a055ff4a
rpf991 <rpf9743@gmail.com>
2026-09-04 17:58:58 -0300

    aba_chat

--- app/app.py
+++ app/app.py
@@ -1,25 +1,48 @@
 import streamlit as st
+import time
 
-st.set_page_config(layout="wide")
+st.set_page_config(
+    page_title="Comparativo de Assistentes de IA (Saude)",
+    layout="wide"
+)
 
+# Definição do Título e do Subtítulo
 st.title("Vital AI (Vital)")
 st.write("Avalie os Modelos")
 
-model_ready, messages, loss_story = False, [], []
 
+# Criação de Variáveis de Sessão do StreamLit
 
+# Preparação do Modelo
+if 'model_ready' not in st.session_state:
+    st.session_state.model_ready = False
 
+# Criação do Historico de Mensagens
+if 'messages' not in st.session_state:
+    st.session_state.messages = []
+
+# Criação do Historico de Perda
+if 'loss_story' not in st.session_state:
+    st.session_state.loss_story = []
+
+# Validação de qual botão executar
 if 'acao_ativa' not in st.session_state:
     st.session_state.acao_ativa = ''    
 
 
-# Criação do Menu de Opções
-with st.sidebar:
-    st.title('Menu Principal')
 
-    st.header("Parâmetros")
+# Menu de Opções 
+barra_opcoes = st.sidebar
 
-    epochs = st.number_input(
+with barra_opcoes:
+    barra_opcoes.title('Menu Principal')
+    barra_opcoes.header("Parâmetros")
+
+    # Recuperação do arquivo principal
+    arquivo_principal = barra_opcoes.file_uploader("Escolha sua base de dados", type='jsonl')
+
+    # Definição da Quantidade de Épocas
+    epochs = barra_opcoes.number_input(
         label="Defina a Quantidade de Épocas", 
         min_value=0.0,
         max_value=100.0,
@@ -27,7 +50,7 @@ with st.sidebar:
         step=1.0
     )
 
-    learning_rate = st.number_input(
+    learning_rate = barra_opcoes.number_input(
         "Defina a Taxa de Aprendizado",
         min_value=0.0,
         max_value=100.0,
@@ -36,12 +59,12 @@ with st.sidebar:
         format="%.2f"
     )
 
-    batch_size = st.selectbox(
+    batch_size = barra_opcoes.selectbox(
         "Defina a Quantidade de Lotes",
-        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     )
 
-    temperature_slider = st.slider(
+    temperature_slider = barra_opcoes.slider(
         label='Defina a Temperatura do Modelo',
         min_value=0.0,
         max_value=100.0,
@@ -49,7 +72,7 @@ with st.sidebar:
         format="%.2f"
     )
 
-    penality_slider = st.slider(
+    penality_slider = barra_opcoes.slider(
         label='Defina a Penalidade do Modelo',
         min_value=0.0,
         max_value=100.0,
@@ -57,22 +80,115 @@ with st.sidebar:
         format="%.2f"
     )
 
-    botao_treinar = st.button("Treinar")
-    botao_retreinar = st.button("Re-Treinar")
+    botao_treinar = barra_opcoes.button("Treinar")
+    botao_retreinar = barra_opcoes.button("Re-Treinar")
 
 
+# Validação do Treinamento
 if botao_treinar:
     st.session_state.acao_ativa = 'botao_treinar' # persistência na escolha do botão treinar
 elif botao_retreinar:
     st.session_state.acao_ativa = 'botao_retreinar' # persistência na escolha do botão re-treinar
 
 
+
+# Logica de Treinar
 if st.session_state.acao_ativa == 'botao_treinar':
-    st.subheader("Treinando")
-    print(f'BatchSize = {batch_size}')
+
+    try:
+        if arquivo_principal is None:
+            # Criação de uma mensagem de alerta
+            placeholder = barra_opcoes.empty()
+            placeholder.info("Precisa de um arquivo para realizar o treinamento")
+
+            # Intervalo de tempo para retirar o aviso prévio
+            time.sleep(5)
+
+            # Remoção da mensagem de alerta
+            placeholder.empty()
+        else:
+            # train_model()
+            st.session_state.model_ready=True
+
+    except Exception as e:
+        barra_opcoes.error(f'Erro durante o treinamento: {str(e)}')
+        st.session_state.model_ready=False
+
     st.session_state.acao_ativa = ''
 
+# Logica de Re-Treinar
 elif st.session_state.acao_ativa == 'botao_retreinar':
-    st.subheader("Re-Treinando")
-    print(f'BatchSize ReTreinamento = {batch_size}')
+    st.session_state.model_ready=False
+
+    # Avaliar se o arquivo existe
+    if arquivo_principal is None:
+        # Criação de uma mensagem de alerta
+        placeholder = barra_opcoes.empty()
+        placeholder.info("Precisa de um arquivo para realizar o re-treinamento")
+
+        # Intervalo de tempo para retirar o aviso prévio
+        time.sleep(5)
+
+        # Remoção da mensagem de alerta
+        placeholder.empty()
+    else:
+        st.session_state.model_ready=True
+
     st.session_state.acao_ativa = ''
+
+
+
+
+# Abas
+aba_chat, aba_treinamento, aba_analise = st.tabs(['💬 Chat', '📊 Treinamento', '📈 Análise'])
+
+with aba_chat:
+    st.header("Discussão com o Assistente")
+
+    # Definição do Layout Padrão do ChatBot
+    col1, col2 = st.columns([6,1])
+
+    # Logica do Botão de Remoção do Histórico de Mensagens
+    with col2:
+        if st.button("Limpar Histórico", use_container_width=True):
+            st.session_state.messages = [] # Limpeza do Histórico
+            st.rerun() # Recarregamento da página após a limpeza
+
+
+    # Avalia se o modelo está pronto para responder
+    if st.session_state.model_ready == False:
+        st.warning("O Modelo não está pronto para responder por favor adquira os dados e realize o treinamento")
+    else:
+        # Criação do container de mensagens
+        message_container = st.empty()
+
+        # Exibição do Histórico de Mensagens
+        with message_container.container():
+            for msg in st.session_state.messages:
+                with st.chat_message(msg['role']):
+                    st.markdown(msg['content'])
+
+
+        # Execução do Prompt de Comando
+        if prompt := st.chat_input("Escreva Sua Pergunta"):
+
+            # Exibe e salva a pergunta do usuário
+            st.session_state.messages.append({'role': 'user', 'content': prompt})
+
+            # Atualização do container com novas mensagens
+            with message_container.container():
+                for msg in st.session_state.messages:
+                    with st.chat_message(msg['role']):
+                        st.markdown(msg['content'])
+
+
+            # Exibe e salva a resposta do usuário
+            resposta = "A resposta é: "
+            st.session_state.messages.append({'role': 'assistant', 'content': resposta})
+
+            # Atualizar novamente o container
+            with message_container.container():
+                for msg in st.session_state.messages:
+                    with st.chat_message(msg['role']):
+                        st.markdown(msg['content'])
+
